19bb6b81f517f7f1c1bd0769b25caba9bb869dc2
Karim Hegab <156906398+KarimHegab698@users.noreply.github.com>
2024-05-02 22:23:28 +0300

    Hi :)

--- Add.v
+++ Add.v
@@ -1,9 +1,40 @@
+`timescale 1ns/100ps
 module Add (
-    input signed [2:0] A,
-    input signed [2:0] B,
-    output reg signed[3:0] C
+    input [2:0] num1,
+    input [2:0] num2,
+    output reg [3:0] result,
+    output reg zeroflag
 );
 always @(*) begin
-    C=A+B;
+    if(!(num1[2]^num2[2]))begin
+        result[2:0]=num1[1:0]+num2[1:0];
+        result[3]=num1[2]&num2[2];
+    end
+    else begin
+        if(num1[2]==1)begin
+            result[2:0]=(~(num1[1:0])+num2[1:0]+1'b1);
+        end
+        else begin
+            result[2:0]=(num1[1:0]+~(num2[1:0])+1'b1);
+        end
+        #100
+        if(result[2]==1)begin
+            result[3]=0;
+        end
+        else begin
+            result[3]=1'b1;
+            result[2:0]=~result[2:0]+1'b1;
+        end
+        result[2]=0;
+    end
+    if(num1[1:0]==0&&num2[1:0]==0)begin
+        result[2:0]=0;
+    end
+    if(result[2:0]==3'b000)begin
+        zeroflag=1'b1;
+    end
+    else begin
+        zeroflag=0;
+    end
 end
 endmodule
--- Add_tb.v
+++ Add_tb.v
@@ -1,27 +1,37 @@
 `timescale 1ns/100ps
 module Add_tb;
-reg signed [2:0] A;
-reg signed [2:0] B;
-wire signed [3:0] C;
+reg [2:0] num1;
+reg [2:0] num2;
+wire [3:0] result;
+wire zeroflag;
 
 Add DUT
 (
-    .A(A),
-    .B(B),
-    .C(C)
+    .num1(num1),
+    .num2(num2),
+    .result(result),
+    .zeroflag(zeroflag)
 );
 integer i;
 integer j;
 integer fd;
 initial begin
     fd=$fopen("add.txt","w");
-    for(i=-3;i<=3;i=i+1) begin
-        for(j=-3;j<=3;j=j+1) begin
-            A=i[2:0];
-            B=j[2:0];
-            #10;
-            $fdisplay(fd,"A(%d)+B(%d)=C(%d)",A,B,C);
-            $display("A(%d)+B(%d)=%d",A,B,C);
+    for(i=3'b000;i<=3'b111;i=i+1) begin
+        for(j=3'b000;j<=3'b111;j=j+1) begin
+            num1=i[2:0];
+            num2=j[2:0];
+            #1000
+            $fdisplay(fd,"Testing Num1=%b and Num2=%b",num1,num2);
+            $display("Testing Num1=%b and Num2=%b",num1,num2);
+            if(zeroflag==1)begin
+                $fdisplay(fd,"%b + %b = %b       Zero Result",num1,num2,result);
+                $display("%b + %b = %b       Zero Result",num1,num2,result);
+            end
+            else begin
+                $fdisplay(fd,"%b + %b = %b",num1,num2,result);
+                $display("%b + %b = %b",num1,num2,result); 
+            end
         end
     end
     $finish();
