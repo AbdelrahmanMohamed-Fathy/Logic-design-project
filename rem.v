4c849f9da531080ee3cac5340ad7d4efa615be2d
Abdelrahman Fathy <abdelrahman.fathy04@outlook.com>
2024-05-04 20:18:42 +0300

    remainder made "scaleable" and working correctly (Tested)

--- rem.v
+++ rem.v
@@ -5,12 +5,6 @@ module rem(
     output reg divbyzero
 );
 always@(*) begin
-
-
-    //! This is currently not scaleable
-    //TODO make this scaleable
-
-
     if (denominator[1:0]==2'b00) begin
         divbyzero=1;
         remainder[2] = numerator[2];
@@ -18,23 +12,13 @@ always@(*) begin
     end
     else begin
         divbyzero=0;
-        case (numerator[0])
-            1'b1: remainder[0] = denominator[1];
-            1'b0: remainder[0] = 1'b0;
-            default: remainder[0] = 1'b0;
+        case(numerator[1:0])
+            2'b01:begin remainder[0] = denominator[1];                      remainder[1] = 0;                                 end
+            2'b10:begin remainder[0] = 0;                                   remainder[1] = denominator[1] && denominator[0];  end
+            2'b11:begin remainder[0] = denominator[1] && !denominator[0];   remainder[1] = 0;                                 end
+            default:remainder[1:0]=2'b00;
         endcase
-        case (numerator[1] && denominator[0])
-            1'b1: remainder[1] = denominator[1];
-            1'b0: remainder[1] = 1'b0; 
-            default: remainder[1] = 1'b0;
-        endcase
-        case (!(numerator[2] ^ denominator[2]))
-            1'b1: remainder[2] = 0;
-            1'b0: remainder[2] = 1;
-            default: remainder[2] = 0;
-        endcase
-        if (remainder[1] && remainder[0])
-            remainder=3'b000;
+        remainder[2]=(numerator[2]^denominator[2]);
     end
 end
 endmodule
