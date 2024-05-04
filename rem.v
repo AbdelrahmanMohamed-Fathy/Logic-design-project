d0041217a6a43eceefa7b23a67922454b1fc2c8d
Abdelrahman Fathy <abdelrahman.fathy04@outlook.com>
2024-05-05 00:08:47 +0300

    finished all except alu

--- rem.v
+++ rem.v
@@ -8,6 +8,7 @@ module rem(
 );
 assign remainder[3:2] = 2'b00;
 always@(*) begin
+    zero=0;
     if (denominator[1:0]==2'b00) begin
         divbyzero=1;
         remainder[1:0]=2'b00;
@@ -15,11 +16,14 @@ always@(*) begin
     else begin
         divbyzero=0;
         case(numerator[1:0])
-            2'b01:  begin remainder[0] = denominator[1];                      remainder[1] = 0;                                zero = 0;    end
-            2'b10:  begin remainder[0] = 0;                                   remainder[1] = denominator[1] && denominator[0]; zero = 0;    end
-            2'b11:  begin remainder[0] = denominator[1] && !denominator[0];   remainder[1] = 0;                                zero = 0;    end
-            default:begin remainder[0] = 0;                                   remainder[1] = 0;                                zero = 1;    end
+            2'b01:  begin remainder[0] = denominator[1];                      remainder[1] = 0;                                   end
+            2'b10:  begin remainder[0] = 0;                                   remainder[1] = denominator[1] && denominator[0];    end
+            2'b11:  begin remainder[0] = denominator[1] && !denominator[0];   remainder[1] = 0;                                   end
+            default:begin remainder[0] = 0;                                   remainder[1] = 0;                                   end
         endcase
+        if(remainder[1:0]==2'b00)begin
+            zero=1;
+        end
     end
     remainder[4] = numerator[2];
 end
